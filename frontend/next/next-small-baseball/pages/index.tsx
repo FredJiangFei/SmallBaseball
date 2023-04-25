1cdc9bf6e0cbfa5d9c3f1c07b17046f8b1845142
Fred Jiang <jiangf@shinetechsoftware.com>
2023-04-25 18:09:37 +0800

    metadata

--- frontend/next/next-small-baseball/pages/index.tsx
+++ frontend/next/next-small-baseball/pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
       <h1 className={styles.title}>Home Page</h1>
       <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
@@ -17,6 +22,6 @@ export default function Home() {
       <a href="/todo" className={styles.btn}>
         Todos
       </a>
-    </div>
+    </>
   );
 }
