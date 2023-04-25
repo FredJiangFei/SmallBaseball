f0633e578403c3b1641e7c7e34056daf6011c56c
Fred Jiang <jiangf@shinetechsoftware.com>
2023-04-25 18:00:02 +0800

    styles

--- frontend/next/next-small-baseball/pages/index.tsx
+++ frontend/next/next-small-baseball/pages/index.tsx
@@ -1,12 +1,22 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import styles from '../styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
+        impedit suscipit architecto, odio inventore nostrum non neque dicta.
+        Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
+        nobis odit.
+      </p>
+      <a href="/todo" className={styles.btn}>
+        Todos
+      </a>
     </div>
   );
 }
