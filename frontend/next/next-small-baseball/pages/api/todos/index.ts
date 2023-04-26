7ffb7ec832a2d142b72ba03302945da3f96f5d2c
Fred Jiang <jiangf@shinetechsoftware.com>
2023-04-26 15:43:50 +0800

    use real data

---
+++ frontend/next/next-small-baseball/pages/api/todos/index.ts
@@ -0,0 +1,24 @@
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from '../../../helpers/db-util';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let client = await connectDatabase();
+
+  if (req.method === 'POST') {
+    await insertDocument(client, 'todos', req.body);
+    res.status(201).json({ message: 'Added success.' });
+  }
+
+  if (req.method === 'GET') {
+    const documents = await getAllDocuments(client, 'todos', { _id: -1 });
+    res.status(200).json({ data: documents });
+  }
+
+  client.close();
+}
+
+export default handler;
--- frontend/next/next-small-baseball/pages/todo/index.tsx
+++ frontend/next/next-small-baseball/pages/todo/index.tsx
@@ -1,19 +1,48 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-export const getStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data = await res.json();
+function TodoList() {
+  const [todos, setTodos] = useState<any[]>([]);
 
-  return {
-    props: { todos: data },
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const response = await fetch('/api/todos/');
+    const data = await response.json();
+    setTodos(data.data);
   };
-};
 
-function TodoList({ todos }: any) {
+  const todoInputRef = useRef<any>();
+
+  function sendTodoHandler(event: any) {
+    event.preventDefault();
+
+    const todo = todoInputRef.current.value;
+
+    fetch('/api/todos/', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: todo,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then(() => fetchTodos());
+  }
+
   return (
     <div>
       <h1>TodoList</h1>
+
+      <form onSubmit={sendTodoHandler}>
+        <input ref={todoInputRef} />
+        <button>Submit</button>
+      </form>
+
       {todos.map((todo: any) => (
         // <Link href={'/todo/' + todo.id} key={todo.id}>
         //   <h3>{todo.title}</h3>
