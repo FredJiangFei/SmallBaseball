1e3d8d43820bdac8b3b1545253b927df9603203b
Fred Jiang <jiangf@shinetechsoftware.com>
2023-06-16 10:19:59 +0800

    add validate

--- backend/nodejs/middleware/auth.js
+++ backend/nodejs/middleware/auth.js
@@ -5,8 +5,6 @@ module.exports = function (req, res, next) {
   const token = req.header('Authorization');
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
-  console.log(token);
-
   try {
     const key = config.get('jwtPrivateKey');
     const tokenFormat  = token.replace("Bearer ", "");
--- backend/nodejs/routes/todos.js
+++ backend/nodejs/routes/todos.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const validate = require('../middleware/validate');
+const validateObjectId = require('../middleware/validateObjectId');
+const Joi = require('joi');
 
 router.get('/', [auth], async (req, res) => {
   const todos = await Todo.find({});
@@ -11,12 +14,12 @@ router.get('/', [auth], async (req, res) => {
   });
 });
 
-router.post('/', [auth], async (req, res) => {
+router.post('/', [auth, validate(validateTodo)], async (req, res) => {
   let todo = await Todo.add(req.body);
   res.send(todo);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
   const todo = await Todo.findByIdAndRemove(req.params.id);
 
   if (!todo)
@@ -24,5 +27,13 @@ router.delete('/:id', [auth, admin], async (req, res) => {
 
   res.send(todo);
 });
+
+function validateTodo(req) {
+  const schema = Joi.object({
+    title: Joi.string().min(5).max(50).required(),
+  });
+
+  return schema.validate(req);
+}
 
 module.exports = router;
